57d8320d695c5ca2f64f367d02458fd07de85b52
amjad992 <amjad.majed992@gmail.com>
2018-07-06 04:17:41 +0300

    adding github links for some projects

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,8 @@
 $(document).ready(function() {
 
     new WOW().init();
-    
-    
+
+
     // Hide and show navigation bar depending of header visibility or not
     $(window).scroll(function() {
         if($(window).scrollTop() < 80 ) {
@@ -17,11 +17,11 @@ $(document).ready(function() {
             });
         }
     });
-    
+
 
     // Add smooth scrolling
     $('.nav-item, #scroll-to-top').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        if (location.pathname.replace('/^\//,') == this.pathname.replace('/^\//,') && location.hostname == this.hostname) {
           var target = $(this.hash);
           target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
           if (target.length) {
@@ -33,7 +33,7 @@ $(document).ready(function() {
         }
       });
 
-    
+
 
     // Activate the menu item on click
     $('.navbar-nav li a').click(function(){
@@ -50,7 +50,7 @@ $(document).ready(function() {
             var sectionId = $(this).attr("id");
             var height = $(this).outerHeight();
             var greaterTop = $(this).offset().top - 150;
-                        
+
             if ($(window).scrollTop() > greaterTop && $(window).scrollTop() < greaterTop + height) {
                 $(".navbar-nav li a[href='#" + sectionId + "']").parent().addClass("active");
             } else {
@@ -58,42 +58,46 @@ $(document).ready(function() {
             }
         });
     });
-    
-    
-    
+
+
+
     // BX Slider for the portfolio
     $('.bxslider').bxSlider({
         maxWidth: 500,
         maxHeight: 500,
         auto: true,
-        minSlides: 1, 
-        maxSlides: 1, 
+        minSlides: 1,
+        maxSlides: 1,
         keyboardEnabled :true,
     });
 });
 
-// Validate the Email and check if the that was on purpose
-var emailIntendedToBeInvalid = false;
-function validateEmail(email) {  
- if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == false
-    && emailIntendedToBeInvalid == false)  
-  {
-      // inform user
-      alert("The Email Address you entered does not appear to be valid. if it was entered invalid intentionaly then you can igonre this, otherwise please go back and fix it if you are expecting a reply to it!");
-  }        emailIntendedToBeInvalid = true;
+// // Validate the Email and check if the that was on purpose
+// var emailIntendedToBeInvalid = false;
+// function validateEmail(email) {
+//  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == false
+//     && emailIntendedToBeInvalid == false)
+//   {
+//       // inform user
+//       alert("The Email Address you entered does not appear to be valid. if it was entered invalid intentionaly then you can igonre this, otherwise please go back and fix it if you are expecting a reply to it!");
+//   }        emailIntendedToBeInvalid = true;
 
-}  
+// }
+
+// var tr = true;
+// var x = (tr:x="true":x="false");
+// console.log(x);
 
 // Submit to the server to email it without moving to the php page
 function submitIt () {
-    
+
     var result;
 
     var formName = document.getElementById("name").value;
     var formEmail = document.getElementById("email").value;
     var formSubject = document.getElementById("subject").value;
     var formMessage = document.getElementById("message").value;
-    
+
     var ajaxRequest = $.ajax(
                     {
                         url: 'send_form_email.php',
@@ -106,7 +110,7 @@ function submitIt () {
                         },
                         async: false
                     });
-    
+
     var resultObj = JSON.parse(result);
 
 
@@ -120,3 +124,4 @@ function submitIt () {
         alert(resultObj.resultMessage);
     });
 }
+
